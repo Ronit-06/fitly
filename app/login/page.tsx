21a43c28403ae2f1d72e26271e047ddf80a90a2c
Ronit Tetarbe <TetarbeR@cardiff.ac.uk>
2025-06-21 00:02:42 +0400

    started work on login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,37 @@
-require('dotenv').config();
+"use client"
 
-export default function login(){
-    console.log(process.env.MONGODB_URI)
-    return(
-        <h1>Login Pagee</h1>
-        
+import { signIn } from "next-auth/react"
+import { useState } from "react"
+
+export default function login() {
+
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSumbit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const result = await signIn("credentials", {
+            email,
+            password,
+            redirect: true,
+            callbackUrl: "/dashboard"
+        });
+
+    };
+
+    return (
+        <>
+            <h1>Login Page</h1>
+            <form onSubmit={handleSumbit}>
+                <label htmlFor="email"> Email</label>
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required></input>
+                <label htmlFor="password">Password</label>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required></input>
+                <button type="submit">Submit</button>
+            </form>
+        </>
     )
 }
 
