c8ccb0a486bc2c734aee9f5e1940f02b5b8af5ef
Ronit Tetarbe <TetarbeR@cardiff.ac.uk>
2025-07-15 18:43:55 +0400

    touch up

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-export default function page(){
-    return(
-        <h1>Base Page</h1>
-    )
+import { redirect } from 'next/navigation';
+
+export default function Page() {
+  redirect('/login');
 }
